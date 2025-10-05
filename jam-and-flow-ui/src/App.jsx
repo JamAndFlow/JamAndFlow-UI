33f31cd183aab93b927ec159823006487c6474ae
KevinRawal <kevinrawal30@gmail.com>
2025-10-05 19:12:48 +0530

    setting up routing in app

--- jam-and-flow-ui/src/App.jsx
+++ jam-and-flow-ui/src/App.jsx
@@ -1,32 +1,23 @@
-import { useEffect, useState } from "react";
 import ThemeToggle from "./components/ThemeToggle";
 import { ThemeProvider } from "./context/ThemeContext";
+import { Routes, Route, NavLink  } from "react-router-dom";
 
-const apiBase = import.meta.env.VITE_API_URL;
+import Home from "./Home";
+import About from "./About";
 
 function App() {
-  const [apiResponse, setApiResponse] = useState(null);
-
-  useEffect(() => {
-    fetch(`${apiBase}/health`)
-      .then(res => res.json())
-      .then(data => setApiResponse(data))
-      .catch(err => console.error("API fetch error:", err));
-  }, []);
-
   return (
     <ThemeProvider>
       <>
-        <div>
-          <h1>Jam and Flow UI</h1>
-          <p>Welcome to the Jam and Flow UI application!</p>
-        </div>
-        <div>
-          <p>API Base URL: {apiBase}</p>
-        </div>
-        <div>
-          <p>Response from API: {apiResponse ? JSON.stringify(apiResponse) : "Loading..."}</p>
-        </div>
+      {/* TODO: use navbar component */}
+      <nav style={{ marginBottom: '20px' }}>
+        <NavLink to="/">Home</NavLink> |{" "}
+        <NavLink to="/about">About</NavLink> |{" "}
+      </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
         <ThemeToggle />
       </>
     </ThemeProvider>
