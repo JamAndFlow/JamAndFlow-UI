79728f40ac2ebb9ce47ed90a7b8c4af95bb2cc31
Kevin Rawal <84058124+kevinrawal@users.noreply.github.com>
2025-10-11 18:43:20 +0530

    setting up routing in app (#8)

---
+++ jam-and-flow-ui/src/About.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+// TODO: remove this component
+const About = () => (
+  <div>
+    <h1>About Jam and Flow UI</h1>
+  </div>
+);
+export default About;
---
+++ jam-and-flow-ui/src/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Jam and Flow UI</h1>
+            <p>Welcome to the Jam and Flow UI application!</p>
+        </div>
+
+    );
+};
+export default Home;
